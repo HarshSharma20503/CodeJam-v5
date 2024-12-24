dbfb209e1ab28f9d16d3000cb008dc7fba2394db
Khushwant <khushwantsingh638@gmail.com>
2024-12-25 03:39:27 +0530

    added get request in Homepage

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,5 +1,21 @@
+import { GetApiCall } from "../utils/apiCall";
+import { useState } from "react";
+
+let [data, setData] = useState([]);
+let [loading, setLoading] = useState(true);
+
+const fetchData = async () => {
+  const response = await GetApiCall("http://localhost:8000/api/user/").then(
+    (data) => console.log(data)
+  );
+  setData(response);
+  setLoading(false);
+};
+
 const Homepage = () => {
-  return <div className="h-screen bg-slate-700">Homepage</div>;
+  return <div className="h-screen bg-slate-700">
+    Homepage
+    </div>;
 };
 
 export default Homepage;
